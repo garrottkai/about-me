b6d8593180c31838706aa702ac43843f73406540
Kai Garrott <garrottkai@gmail.com>
2016-04-13 14:16:24 -0600

    remove css reference

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 	<html>
 	<head>
 		<meta charset="utf-8" />
-		<link type="text/css" href="stylesheet.css" rel="stylesheet" />
 		<title>About Me</title>
 
 	</head>
 	<body>
 		<h1 id="main-heading">Hello!</h1>
 		<p id="intro-paragraph">My name is Kai Garrott.<br>
-			I'm originally from the Midwest, but I've lived in New Mexico for the last six years. I enjoy hiking, camping, flying paragliders, and <em>especially</em> learning new things.</p>
-		<img src="IMG_5463c.JPG" height=550px alt="a photo of me" />
+			I'm originally from the Midwest, but I've lived in New Mexico for the last six years.<br>
+			I enjoy hiking, camping, traveling, flying paragliders, and <em>especially</em> learning new things.</p>
+		<img src="images/IMG_5463c.JPG" height=550px alt="a photo of me" />
 	</body>
 </html>
