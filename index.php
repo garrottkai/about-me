f9f3e74f63aacfab35af17c4b30f788451759407
Kai Garrott <garrottkai@gmail.com>
2016-04-13 13:38:50 -0600

    Implement style sheet

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 	<html>
 	<head>
 		<meta charset="utf-8" />
+		<link type="text/css" href="stylesheet.css" rel="stylesheet" />
 		<title>About Me</title>
+
 	</head>
 	<body>
-		<h1 id="main-heading">Hello</h1>
-		<p id="intro-paragraph">My name is Kai</p>
+		<h1 id="main-heading">Hello!</h1>
+		<p id="intro-paragraph">My name is Kai Garrott.</p>
 	</body>
 </html>
