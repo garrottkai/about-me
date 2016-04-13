a994a7506857b07c49fc34832d6c5e48dbe34c6a
Kai Garrott <garrottkai@gmail.com>
2016-04-13 14:00:25 -0600

    Add image

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 	</head>
 	<body>
 		<h1 id="main-heading">Hello!</h1>
-		<p id="intro-paragraph">My name is Kai Garrott.</p>
+		<p id="intro-paragraph">My name is Kai Garrott.<br>
+			I'm originally from the Midwest, but I've lived in New Mexico for the last six years. I enjoy hiking, camping, flying paragliders, and especially learning new things.</p>
+		<img src="IMG_5463c.JPG" alt="a photo of me" />
 	</body>
 </html>
